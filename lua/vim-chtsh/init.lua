6ec55dffe3c741ff27964313f6f35b385202548d
b72u68 <mdinh@hawk.iit.edu>
2021-01-18 14:25:14 -0800

    Create new CheatPaste function

--- lua/vim-chtsh/init.lua
+++ lua/vim-chtsh/init.lua
@@ -9,65 +9,86 @@ else
     vimcmd = vim.command
 end
 
-local function getSearchString()
-    local search = vim.fn.input("Cheat Sheet > ")
-    search = search:gsub("% ", "+")
-    return search
+local window_settings = vim.g["chtsh_window_settings"]
+
+local function getSearchStringAndCommand(include_comments)
+    local searchQuery = vim.fn.input("Cheat Sheet > ")
+    searchQuery = (searchQuery:gsub("% ", "+")):gsub("%s+", "")
+
+    local command
+
+    if searchQuery ~= "" and searchQuery ~= nil then
+        local url = string.format("%s/%s/%s", baseUrl, filetype, searchQuery)
+
+        if include_comments == nil then
+            include_comments = vim.g["chtsh_include_comments"]
+        end
+
+        if include_comments == 0 then
+            command = "r !curl --silent " .. (url .. "\\?QT")
+        else
+            command = "r !curl --silent " .. (url .. "\\?T")
+        end
+    end
+
+    return command
 end
 
 local function createFloatingWindow()
     local stats = vim.api.nvim_list_uis()[1]
-    local width = stats.width
-    local height = stats.height
-    local offSet = 10
+
+    local width = math.floor(stats.width * window_settings["width"][false])
+    local height = math.floor(stats.height * window_settings["height"][false])
+    local col = math.floor((stats.width - width) / 2)
+    local row = math.floor((stats.height - height) / 2)
 
     local bufh = vim.api.nvim_create_buf(false, true)
     local winId = vim.api.nvim_open_win(bufh, true, {
         relative = "editor",
-        width = width - offSet * 4,
-        height = height - offSet,
-        col = offSet * 2,
-        row = offSet / 2,
+        width = width,
+        height = height,
+        col = col,
+        row = row,
         style="minimal"
     })
-
-    vimcmd("augroup LeaveWindow | autocmd BufLeave * :q! | autocmd! LeaveWindow | augroup END")
+    return winId
 end
 
 local function displayResultInWindow(command)
-    createFloatingWindow()
+    local winId = createFloatingWindow()
 
-    local result = vim.api.nvim_exec(command, true)
-    result = (result:gsub("^:!curl [^\n]*\n", "")):gsub("%s+", "")
+    if winId ~= 0 then
+        vimcmd("augroup LeaveWindow | autocmd BufLeave * :q! | autocmd! LeaveWindow | augroup END")
 
-    if result == '' or result == nil then
-        vimcmd("r !echo \" No Result Found \"")
+        local result = vim.api.nvim_exec(command, true)
+        result = (result:gsub("^:!curl [^\n]*\n", "")):gsub("%s+", "")
+
+        if result == '' or result == nil then
+            vimcmd("r !echo \" No Result Found \"")
+        else
+            vimcmd("set filetype=" .. filetype)
+        end
+
+        vimcmd("set wrap | 1 | 1,1d")
+
     else
-        vimcmd(string.format("set filetype=%s", filetype))
+        print("Error Creating Windows")
     end
-
-    vimcmd("set wrap | 1 | 1,1d")
 end
 
 local function writeResultUnderCursor(command)
     vimcmd(command)
 end
 
-local function search()
-    local searchQuery = (getSearchString()):gsub("%s+", "")
+local function cheat(include_comments, result_under_cursor)
+    local command = getSearchStringAndCommand(include_comments)
 
-    if searchQuery ~= "" and searchQuery ~= nil then
-        local url = string.format("%s/%s/%s", baseUrl, filetype, searchQuery)
-
-        local command
-
-        if vim.g["chtsh_include_comments"] == 0 then
-            command = string.format("r !curl --silent %s", (url .. "\\?QT"))
-        else
-            command = string.format("r !curl --silent %s", (url .. "\\?T"))
+    if command ~= nil then
+        if result_under_cursor == nil then
+            result_under_cursor = vim.g["chtsh_result_under_cursor"]
         end
 
-        if vim.g["chtsh_result_under_cursor"] == 0 then
+        if result_under_cursor == 0 then
             displayResultInWindow(command)
         else
             writeResultUnderCursor(command)
@@ -76,5 +97,5 @@ local function search()
 end
 
 return {
-    search = search
+    cheat = cheat
 }
--- plugin/vim-chtsh.vim
+++ plugin/vim-chtsh.vim
@@ -1,12 +1,17 @@
 let g:chtsh_include_comments = 1
 let g:chtsh_result_under_cursor = 0
+let g:chtsh_window_settings = { "width": 0.7, "height": 0.7 }
 
 fun! CheatSheet()
     lua for k in pairs(package.loaded) do if k:match("^vim%-chtsh") then package.loaded[k] = nil end end
-    lua require("vim-chtsh").search()
+    lua require("vim-chtsh").cheat()
+endfun
+
+fun! CheatPaste()
+    lua for k in pairs(package.loaded) do if k:match("^vim%-chtsh") then package.loaded[k] = nil end end
+    lua require("vim-chtsh").cheat(nil, 1)
 endfun
 
 augroup CheatSheet
     autocmd!
-    nnoremap <leader>ch :call CheatSheet()<CR>
 augroup END
